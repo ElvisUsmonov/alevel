73868056b333e2b58699ade412fccda0fd41b51c
ElvisUsmonov <146858238+ElvisUsmonov@users.noreply.github.com>
2023-10-12 21:54:38 +0300

    Update Lesson2Ex4.java

--- src/Lesson2Ex4.java
+++ src/Lesson2Ex4.java
@@ -2,26 +2,25 @@ import java.util.Scanner;
 
 public class Lesson2Ex4 {
     public static void main(String[] args) {
-        float a = 0;
-        float b = 0;
-        float c = 0;
+        theSmallest();
+    }
 
+    public static void theSmallest() {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Введите первое число: ");
-        a = input.nextFloat();
+        double a = input.nextFloat();
 
         System.out.print("Введите второе число: ");
-        b = input.nextFloat();
+        double b = input.nextFloat();
 
         System.out.print("Введите третье число: ");
-        c = input.nextFloat();
+        double c = input.nextFloat();
 
-        System.out.println((a < b && a < c) ? ("Число " + a + " меньшее"):("Число " + a + " не самое меньшее"));
-
-        System.out.println((b < a && b < c) ? ("Число " + b + " меньшее"):("Число " + b + " не самое меньшее"));
-
-        System.out.println((c < a && c < b) ? ("Число " + c + " меньшее"):("Число " + c + " не самое меньшее"));
+        String theSmallest = Math.abs(a) < Math.abs(b) ?
+                Math.abs(a) < Math.abs(c) ? "Число " + a + " меньшее":
+                "Число " + c + " меньшее" : Math.abs(b) < Math.abs(c) ?
+                "Число " + b + " меньшее" : "Число " + c + " меньшее";
+        System.out.println(theSmallest);
     }
 }
-
