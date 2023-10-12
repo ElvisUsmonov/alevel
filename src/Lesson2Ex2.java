d7f058b709379d30875440f8061d1b2db22684f8
ElvisUsmonov <146858238+ElvisUsmonov@users.noreply.github.com>
2023-10-12 21:52:59 +0300

    Update Lesson2Ex2.java

--- src/Lesson2Ex2.java
+++ src/Lesson2Ex2.java
@@ -1,19 +1,21 @@
 public class Lesson2Ex2 {
     public static void main(String[] args) {
+        System.out.println(triangleArea());
+    }
+
+    public static double triangleArea() {
         double x1 = 1;
         double x2 = 3;
         double x3 = 6;
         double y1 = 2;
         double y2 = 5;
         double y3 = 7;
-
         double a = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         double b = Math.sqrt(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2));
         double c = Math.sqrt(Math.pow(x3 - x1, 2) + Math.pow(y3 - y1, 2));
 
         double p = (a + b + c)/2.0;
         double s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-
-        System.out.println("Площадь = " + s);
+        return s;
     }
 }
